e64fa297c2b1571cf47e66469d61f873639c9f1f
bach916 <phambachdz2005@gmail.com>
2026-01-29 08:15:02 +0700

    update

--- script.js
+++ script.js
@@ -20,13 +20,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBQsh2KAXZP_7WfSC_YUJnrUTgL_qKJmdc",  // copy mới nếu khác
+  apiKey: "AIzaSyBQsh2KAXZP_7WfSC_YUJnrUTgL_qKJmdc",
   authDomain: "phamngocbach-10a0c.firebaseapp.com",
   projectId: "phamngocbach-10a0c",
   storageBucket: "phamngocbach-10a0c.firebasestorage.app",
   messagingSenderId: "554907446263",
-  appId: "1:554907446263:web:839299a8b2b500dc3c9cf2",  // copy mới nếu khác
-  measurementId: "G-0H222HXRCN"  // copy mới nếu khác
+  appId: "1:554907446263:web:839299a8b2b500dc3c9cf2",
+  measurementId: "G-0H222HXRCN"
 };
 
 const app = initializeApp(firebaseConfig);
